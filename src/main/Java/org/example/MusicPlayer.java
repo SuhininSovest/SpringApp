f0592bc2e717b2fe104ebd1edb26da3b89043885
SovestSuhinin <suhinin.sovest@yandex.ru>
2023-07-29 21:04:30 +0300

    First Commit to my test App

--- src/main/Java/org/example/MusicPlayer.java
+++ src/main/Java/org/example/MusicPlayer.java
@@ -6,11 +6,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class MusicPlayer {
     private ClassicalMusic classicalMusic;
-    @Autowired
+
     public MusicPlayer(ClassicalMusic classicalMusic) {
         this.classicalMusic = classicalMusic;
     }
     public void playMusic() {
         System.out.println("Play: " + classicalMusic.getSong());
+    }
+    @Autowired
+    public void setClassicalMusic(ClassicalMusic classicalMusic) {
+        this.classicalMusic = classicalMusic;
     }
 }
